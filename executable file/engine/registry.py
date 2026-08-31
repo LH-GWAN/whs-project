e749604aa1e7bce933d7fdbbf934e8f10bf66e48
LH-GWAN <emqdlfqnrP@gmail.com>
2026-08-31 16:53:18 +0900

    레포 변경

--- executable file/engine/registry.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-ENGINE_MODULE = "integration_blackbox"
-
-ENGINE_NAME = "blackbox"
-
-MODULE_BY_ENGINE = {ENGINE_NAME: ENGINE_MODULE}
-
-NEEDS_EXTRA_TRACK_PASSES = True
